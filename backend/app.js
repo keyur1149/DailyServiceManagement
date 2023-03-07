c47df67217d339b295ee106ff8a51ad1cf0123bd
keyur1149 <keyurpatel1149@gmail.com>
2023-03-07 23:33:32 +0530

    initial   commit
    initial   commit

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+var cors = require('cors')
+const schedule = require('node-schedule');
+
+mongoose.set('strictQuery', true);
+
+const express = require('express');
+const router = require("./routes/customer_routes/auth");
+const app = express();
+dotenv.config({ path: './config.env' });
+require('./db/connect')
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json())
+app.use(cors());
+schedule.scheduleJob('* * * 1-12 *', () => {
+    console.log("working ");
+});
+// app.use(require('./routes/customer_routes/auth'));
+app.use(require('./routes/customer_routes/auth'));
+const port = process.env.port;
+app.listen(port, () => {
+    console.log(`server is running on ${port} port`);
+});
+console.log("moanan")
